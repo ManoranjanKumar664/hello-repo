721f5b447e6b7bbc1abd2dd6381fc0cd964c78b9
Manoranjan Kumar <112553053+ManoranjanKumar664@users.noreply.github.com>
2023-02-21 21:27:52 +0530

    he added

--- file1.java
+++ file1.java
@@ -4,6 +4,6 @@ public class file1
 {
     public static void main(String []args)
     {
-        System.out.println("this is the best boy");
+        System.out.println("He is the best boy");
     }
 }
